b660237271a5bb06229bbc0111597d4e41c58f8d
Jamey <musoyou1085@gmail.com>
2024-05-08 17:04:22 +0900

    first commit

---
+++ src/main/java/com/dev/moim/MoimApplication.java
@@ -0,0 +1,13 @@
+package com.dev.moim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoimApplication.class, args);
+	}
+
+}
