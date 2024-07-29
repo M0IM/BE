8a97126da1862829af0a46228f0b0fa89733dfcd
yewonahn <yewonahn@naver.com>
2024-07-29 17:38:12 +0900

    docs: AuthController 스웨거 ApiResponse 작성 (#42)

--- src/main/java/com/dev/moim/domain/account/controller/AuthController.java
+++ src/main/java/com/dev/moim/domain/account/controller/AuthController.java
@@ -22,35 +22,41 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/join")
-    @Operation(summary="회원 가입 API", description="회원 가입 성공 시, 자동 로그인을 위해 유저 계정 정보 반환" )
+    @Operation(summary="회원 가입 API", description="로그인 타입을 입력해주세요. \n [Provider] LOCAL(일반 로그인), KAKAO, APPLE, GOOGLE" )
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "COMMON201", description = "회원 가입 성공"),
+            @ApiResponse(responseCode = "COMMON201", description = "요청 성공 및 리소스 생성됨"),
+            @ApiResponse(responseCode = "AUTH_011", description = "이미 가입한 메일 입니다.")
     })
     public BaseResponse<JoinResponse> join(@RequestBody JoinRequest request) {
         return BaseResponse.onSuccess(authService.join(request));
     }
 
     @PostMapping("/login")
-    @Operation(summary="로그인 API", description="로그인 성공 시, token 정보 반환" )
+    @Operation(summary="일반 로그인 API", description="로그인 성공 시, token 정보 반환 (개발용)" )
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "COMMON200", description = "로그인 성공"),
+            @ApiResponse(responseCode = "COMMON200", description = "성공입니다."),
+            @ApiResponse(responseCode = "AUTH_009", description = "비밀번호를 잘못 입력했습니다."),
+            @ApiResponse(responseCode = "AUTH_010", description = "인증에 실패했습니다."),
+            @ApiResponse(responseCode = "AUTH_021", description = "존재하지 않는 사용자입니다.")
     })
     public BaseResponse<TokenResponse> login(@RequestBody @Valid LoginRequest request) {
-
         return BaseResponse.onSuccess(null);
     }
 
     @PostMapping("/reissueToken")
     @Operation(summary="토큰 재발급 API", description="AccessToken의 유효 기간이 만료된 경우, Authorization 헤더에 RefreshToken을 담아서 요청을 보내면 AccessToken과 RefreshToken 재발급.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "COMMON200", description = "토큰 재발급 성공"),
+            @ApiResponse(responseCode = "COMMON200", description = "성공입니다."),
+            @ApiResponse(responseCode = "AUTH_001", description = "토큰이 만료되었습니다."),
+            @ApiResponse(responseCode = "AUTH_002", description = "토큰이 유효하지 않습니다."),
+            @ApiResponse(responseCode = "AUTH_021", description = "존재하지 않는 사용자입니다.")
     })
     public BaseResponse<TokenResponse> reissueToken(@ExtractToken @Parameter(name = "refreshToken", hidden = true) String refreshToken) {
         return BaseResponse.onSuccess(authService.reissueToken(refreshToken));
     }
 
     @PostMapping("/logout")
-    @Operation(summary="로그아웃 API", description="로그아웃 후, 기존 유효한 토큰 무효화" )
+    @Operation(summary="로그아웃 API", description="로그아웃 후, 기존 유효한 토큰 무효화 (개발용)" )
     public BaseResponse<?> signOut(
             @ExtractToken @Parameter(name = "accessToken", hidden = true) String accessToken
     ) {
@@ -58,7 +64,14 @@ public class AuthController {
     }
 
     @PostMapping("/oAuth")
-    @Operation(summary="소셜 로그인 API", description="현재는 카카오 로그인만 가능합니다." )
+    @Operation(summary="소셜 로그인 API", description="소셜 로그인 타입을 입력해주세요.\n [Provider] KAKAO, APPLE, GOOGLE (개발용)")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "COMMON200", description = "성공입니다."),
+            @ApiResponse(responseCode = "AUTH_001", description = "신규 유저 입니다. 회원가입을 진행해주세요."),
+            @ApiResponse(responseCode = "AUTH_016", description = "지원하지 않는 로그인 provider 입니다."),
+            @ApiResponse(responseCode = "AUTH_018", description = "만료된 ID 토큰 입니다."),
+            @ApiResponse(responseCode = "AUTH_019", description = "유효하지 않은 ID 토큰 입니다."),
+    })
     public BaseResponse<TokenResponse> oAuthLogin(
             @RequestBody OAuthLoginRequest request
     ) {
