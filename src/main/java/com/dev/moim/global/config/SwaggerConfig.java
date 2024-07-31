8d938554d7050e5c5055f71f8061040cc23d6cf9
Jamey <musoyou1085@gmail.com>
2024-07-31 19:28:53 +0900

    MM-54 docs: moim api 명세서로 교체

--- src/main/java/com/dev/moim/global/config/SwaggerConfig.java
+++ src/main/java/com/dev/moim/global/config/SwaggerConfig.java
@@ -15,8 +15,8 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI UMCstudyAPI() {
         Info info = new Info()
-                .title("UMC Server WorkBook API")
-                .description("UMC Server WorkBook API 명세서")
+                .title("Moim server API")
+                .description("Moim Server API 명세서")
                 .version("1.0.0");
 
         String jwtSchemeName = "JWT TOKEN";
