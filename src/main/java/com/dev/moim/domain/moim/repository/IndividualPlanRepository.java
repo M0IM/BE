0c4415b977b5424896aad823a3d301861cb58a17
yewonahn <yewonahn@naver.com>
2024-08-12 11:43:26 +0900

    feat: 개인 일정 repository 추가 (#78)

---
+++ src/main/java/com/dev/moim/domain/moim/repository/IndividualPlanRepository.java
@@ -0,0 +1,7 @@
+package com.dev.moim.domain.moim.repository;
+
+import com.dev.moim.domain.moim.entity.IndividualPlan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IndividualPlanRepository extends JpaRepository<IndividualPlan, Long> {
+}
