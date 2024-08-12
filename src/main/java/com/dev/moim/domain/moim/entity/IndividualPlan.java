644aeb9ee2bedef4c5177025de3bacdbf1e041ef
yewonahn <yewonahn@naver.com>
2024-08-12 11:43:18 +0900

    feat: 개인 일정 엔티티 추가 (#78)

---
+++ src/main/java/com/dev/moim/domain/moim/entity/IndividualPlan.java
@@ -0,0 +1,32 @@
+package com.dev.moim.domain.moim.entity;
+
+import com.dev.moim.domain.account.entity.User;
+import com.dev.moim.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@DynamicInsert
+@DynamicUpdate
+public class IndividualPlan extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    private LocalDateTime date;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
