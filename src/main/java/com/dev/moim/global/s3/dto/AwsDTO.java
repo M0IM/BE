821dcb024c2fe382be0e813e9b76aa10e574bfb7
Jamey <musoyou1085@gmail.com>
2024-08-13 22:53:04 +0900

    MM-86 feat: presigned url 구현

---
+++ src/main/java/com/dev/moim/global/s3/dto/AwsDTO.java
@@ -0,0 +1,30 @@
+package com.dev.moim.global.s3.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class AwsDTO {
+
+    @Schema(description = "AWS S3 URL 응답 정보")
+    @Builder
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class PresignedUrlUploadResponse {
+        private String url;
+        private String keyName;
+    }
+
+    @Schema(description = "AWS S3 URL 응답 정보")
+    @Builder
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class PresignedUrlDownLoadResponse {
+        private String url;
+    }
+}
