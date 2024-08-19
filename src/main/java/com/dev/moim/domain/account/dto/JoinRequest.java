5043736b35cd414d0e7a185b9564dbcadde0a032
yewonahn <yewonahn@naver.com>
2024-08-20 01:20:12 +0900

    MM-129 feat: 회원가입 DTO birth 타입 변환 (#147)

--- src/main/java/com/dev/moim/domain/account/dto/JoinRequest.java
+++ src/main/java/com/dev/moim/domain/account/dto/JoinRequest.java
@@ -5,6 +5,7 @@ import com.dev.moim.domain.account.entity.enums.Provider;
 import com.dev.moim.global.validation.annotation.LocalAccountValidation;
 import com.dev.moim.global.validation.annotation.PasswordValidation;
 import com.dev.moim.global.validation.annotation.OAuthAccountValidation;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -17,14 +18,20 @@ import java.time.LocalDate;
 @PasswordValidation
 public record JoinRequest(
         @Schema(description = "로그인 타입", defaultValue = "KAKAO", allowableValues = {"LOCAL", "KAKAO", "APPLE", "GOOGLE"})
-        @NonNull Provider provider,
+        @NonNull
+        Provider provider,
         String providerId,
-        @NotBlank String nickname,
-        @NotBlank String email,
+        @NotBlank
+        String nickname,
+        @NotBlank
+        String email,
         String password,
         @Schema(description = "성별", defaultValue = "FEMALE", allowableValues = {"FEMALE", "MALE"})
-        @NonNull Gender gender,
-        @NotNull LocalDate birth,
-        @NotBlank String residence
+        @NonNull
+        Gender gender,
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        LocalDate birth,
+        @NotBlank
+        String residence
 ) {
 }
