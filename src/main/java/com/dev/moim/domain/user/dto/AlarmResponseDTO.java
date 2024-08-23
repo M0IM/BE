369046179a2fc4520386a540fe0af40834b1c0d4
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2024-08-23 17:15:19 +0900

    MM-163 refactor: 알림 목록 조회 시 생성 시간 추가

--- src/main/java/com/dev/moim/domain/user/dto/AlarmResponseDTO.java
+++ src/main/java/com/dev/moim/domain/user/dto/AlarmResponseDTO.java
@@ -2,12 +2,15 @@ package com.dev.moim.domain.user.dto;
 
 import com.dev.moim.domain.account.entity.Alarm;
 
+import java.time.LocalDateTime;
+
 public record AlarmResponseDTO(
         Long alarmId,
         String title,
-        String content
+        String content,
+        LocalDateTime createdAt
 ) {
     public static AlarmResponseDTO toAlarmResponseDTO(Alarm alarm) {
-        return new AlarmResponseDTO(alarm.getId(), alarm.getTitle(), alarm.getContent());
+        return new AlarmResponseDTO(alarm.getId(), alarm.getTitle(), alarm.getContent(), alarm.getCreatedAt());
     }
 }
