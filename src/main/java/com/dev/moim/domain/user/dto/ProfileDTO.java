860e37f6f7cb3e9611067e7fc7cdfc2b61e155f5
yewonahn <yewonahn@naver.com>
2024-08-25 22:12:03 +0900

    MM-175 feat: 유저 프로필 조회 DTO에 provider 추가 (#202)

--- src/main/java/com/dev/moim/domain/user/dto/ProfileDTO.java
+++ src/main/java/com/dev/moim/domain/user/dto/ProfileDTO.java
@@ -2,17 +2,20 @@ package com.dev.moim.domain.user.dto;
 
 import com.dev.moim.domain.account.entity.User;
 import com.dev.moim.domain.account.entity.UserProfile;
+import com.dev.moim.domain.account.entity.enums.Provider;
 
 public record ProfileDTO(
         Long userId,
         String nickname,
-        String imageUrl
+        String imageUrl,
+        Provider provider
 ) {
     public static ProfileDTO of(User user, UserProfile userProfile) {
         return new ProfileDTO(
                 user.getId(),
                 userProfile.getName(),
-                userProfile.getImageUrl()
+                userProfile.getImageUrl(),
+                user.getProvider()
         );
     }
 }
