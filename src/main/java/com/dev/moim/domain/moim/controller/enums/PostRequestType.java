5ae9267d862cb36a11b8d22ca874d823a76e1788
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2024-08-25 09:35:35 +0900

    MM-172 refactor: 일반 post 요청 GLOBAL 없애기

--- src/main/java/com/dev/moim/domain/moim/controller/enums/PostRequestType.java
+++ src/main/java/com/dev/moim/domain/moim/controller/enums/PostRequestType.java
@@ -1,5 +1,5 @@
 package com.dev.moim.domain.moim.controller.enums;
 
 public enum PostRequestType {
-    ANNOUNCEMENT, REVIEW, WELCOME, FREE, ALL, GLOBAL
+    ANNOUNCEMENT, REVIEW, WELCOME, FREE, ALL
 }
