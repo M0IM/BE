3c7c1d8178e258ec4e87f799858dec2b0a720924
yewonahn <yewonahn@naver.com>
2024-08-26 21:46:54 +0900

    MM-183 feat: 모임 소개 게시물 조회 DTO 모임 이미지 추가 (#211)

--- src/main/java/com/dev/moim/domain/moim/dto/post/MoimPostPreviewDTO.java
+++ src/main/java/com/dev/moim/domain/moim/dto/post/MoimPostPreviewDTO.java
@@ -1,15 +1,19 @@
 package com.dev.moim.domain.moim.dto.post;
 
 import com.dev.moim.domain.moim.entity.Post;
+import com.dev.moim.domain.moim.entity.PostImage;
 import com.dev.moim.domain.moim.entity.enums.PostType;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record MoimPostPreviewDTO(
         Long moimPostId,
+        Long moimId,
         String title,
         String content,
-        String profileImage,
+        List<String> moimImageUrl,
+        String ownerProfileImageUrl,
         String writer,
         Integer commentCount,
         Integer likeCount,
@@ -19,8 +23,11 @@ public record MoimPostPreviewDTO(
     public static MoimPostPreviewDTO toMoimPostPreviewDTO(Post post) {
         return new MoimPostPreviewDTO(
                 post.getId(),
+                post.getMoim().getId(),
                 post.getTitle(),
                 post.getContent(),
+                post.getPostImageList() == null ? null : post.getPostImageList()
+                        .stream().map(PostImage::getImageKeyName).toList(),
                 post.getUserMoim() == null ? null : post.getUserMoim().getUserProfile().getImageUrl(),
                 post.getUserMoim() == null ? null : post.getUserMoim().getUserProfile().getName(),
                 post.getCommentList().size(),
