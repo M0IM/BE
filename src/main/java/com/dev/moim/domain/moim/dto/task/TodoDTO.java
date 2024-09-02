1776dd6f74321c4af2efb32bd81ba5bd6ac0a690
yewonahn <yewonahn@naver.com>
2024-09-02 22:39:24 +0900

    MM-235 feat: 모임 todo 조회 DTO에 imageUrlList 추가 (#283)

--- src/main/java/com/dev/moim/domain/moim/dto/task/TodoDTO.java
+++ src/main/java/com/dev/moim/domain/moim/dto/task/TodoDTO.java
@@ -1,15 +1,18 @@
 package com.dev.moim.domain.moim.dto.task;
 
 import com.dev.moim.domain.moim.entity.Todo;
+import com.dev.moim.domain.moim.entity.TodoImage;
 import com.dev.moim.domain.moim.entity.UserMoim;
 import com.dev.moim.domain.moim.entity.enums.MoimRole;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record TodoDTO(
         Long todoId,
         String title,
         LocalDateTime dueDate,
+        List<String> imageUrlList,
         String writerNickname,
         String writerProfileImageUrl,
         MoimRole writerMoimRole,
@@ -22,6 +25,7 @@ public record TodoDTO(
                 todo.getId(),
                 todo.getTitle(),
                 todo.getDueDate(),
+                todo.getTodoImageList().stream().map(TodoImage::getImageUrl).toList(),
                 userMoim.getUserProfile().getName(),
                 userMoim.getUserProfile().getImageUrl(),
                 userMoim.getMoimRole(),
@@ -35,6 +39,7 @@ public record TodoDTO(
                 todo.getId(),
                 todo.getTitle(),
                 todo.getDueDate(),
+                todo.getTodoImageList().stream().map(TodoImage::getImageUrl).toList(),
                 null,
                 null,
                 null,
