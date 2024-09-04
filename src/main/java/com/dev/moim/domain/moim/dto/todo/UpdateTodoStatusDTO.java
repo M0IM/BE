a03e9d6c8e6de82111a80365888e0d75199f5410
yewonahn <yewonahn@naver.com>
2024-09-05 02:16:08 +0900

    MM-246 feat: todo assignee 검증 어노테이션 적용 (#297)

--- src/main/java/com/dev/moim/domain/moim/dto/todo/UpdateTodoStatusDTO.java
+++ src/main/java/com/dev/moim/domain/moim/dto/todo/UpdateTodoStatusDTO.java
@@ -2,10 +2,11 @@ package com.dev.moim.domain.moim.dto.todo;
 
 import com.dev.moim.domain.moim.entity.enums.TodoAssigneeStatus;
 import com.dev.moim.global.validation.annotation.TodoAssigneeStatusValidation;
+import com.dev.moim.global.validation.annotation.TodoAssigneeValidation;
 
 @TodoAssigneeStatusValidation
 public record UpdateTodoStatusDTO(
-        Long todoId,
+        @TodoAssigneeValidation Long todoId,
         TodoAssigneeStatus todoAssigneeStatus
 ) {
 }
