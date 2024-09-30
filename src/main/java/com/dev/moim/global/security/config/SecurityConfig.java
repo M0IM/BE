bd95d4855f0483afd83c83042a54e445bb2024f2
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2024-10-01 00:59:54 +0900

    MM-313 refactor: swagger 서버 열기

--- src/main/java/com/dev/moim/global/security/config/SecurityConfig.java
+++ src/main/java/com/dev/moim/global/security/config/SecurityConfig.java
@@ -129,7 +129,7 @@ public class SecurityConfig {
                 .accessDeniedHandler(jwtAccessDeniedHandler));
 
         http.authorizeHttpRequests(requests -> requests
-                .requestMatchers(Arrays.asList(environment.getActiveProfiles()).contains("release") ? releaseAllowUrls : allowUrls).permitAll()
+                .requestMatchers(allowUrls).permitAll()
                 .requestMatchers("/**").authenticated()
                 .anyRequest().permitAll());
 
@@ -142,8 +142,8 @@ public class SecurityConfig {
 
         http.addFilterAt(customLoginFilter, UsernamePasswordAuthenticationFilter.class);
         http.addFilterAt(oAuthLoginFilter, UsernamePasswordAuthenticationFilter.class);
-        http.addFilterBefore(new JwtFilter(jwtUtil,redisUtil, Arrays.asList(environment.getActiveProfiles()).contains("release") ? releaseAllowUrls : allowUrls), CustomLoginFilter.class);
-        http.addFilterBefore(new JwtExceptionFilter(Arrays.asList(environment.getActiveProfiles()).contains("release") ? releaseAllowUrls : allowUrls), JwtFilter.class);
+        http.addFilterBefore(new JwtFilter(jwtUtil,redisUtil, allowUrls), CustomLoginFilter.class);
+        http.addFilterBefore(new JwtExceptionFilter(allowUrls), JwtFilter.class);
 
         http.logout(logout -> logout
                 .logoutUrl("/api/v1/auth/logout")
